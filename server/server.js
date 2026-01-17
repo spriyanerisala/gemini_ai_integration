7aa0d013d6e696de10f767865c053e5eed5c6df4
siva priya <nspriya005@gmail.com>
2026-01-17 16:05:05 +0530

    cors change

--- server/server.js
+++ server/server.js
@@ -9,7 +9,9 @@ dotenv.config();
 
 const app=express();
 
-app.use(cors());
+app.use(cors({
+    origin:'*'
+}));
 app.use(express.json());
 
 app.use('/api/ai',aiRoutes); //http://localhost:5001/api/ai/ask
